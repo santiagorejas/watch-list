710380cb97e283e3343d36c8fb32a7c4f2d41be7
santiagorejas <santiago.orejasrg@gmail.com>
2021-07-17 01:31:16 -0300

    homepage sections

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -17,10 +17,16 @@ const HomePage = () => {
     <div className={`${classes.homepage}`}>
       <HomeSearch className={`${classes["homepage__search"]}`} />
       <div className={`${classes["homepage__sections"]} section`}>
+        <div className={`${classes["homepage__section"]}`}>
+          <h1 className={classes["homepage__section-title"]}>Popular</h1>
+          <HorizontalList
+            url={
+              "https://api.themoviedb.org/3/movie/popular?api_key=b61e1b3719a9ee56423ad6e473cbf2ab&language=en-US&page=2"
+            }
+          />
+        </div>
         <div className={`{$classes['homepage__section]}`}>
-          <h1 className={classes["homepage__section-title"]}>
-            Titulo de la sección
-          </h1>
+          <h1 className={classes["homepage__section-title"]}>Trending</h1>
           <HorizontalList
             url={
               "https://api.themoviedb.org/3/trending/all/day?api_key=b61e1b3719a9ee56423ad6e473cbf2ab"
@@ -28,50 +34,16 @@ const HomePage = () => {
           />
         </div>
         <div className={classes["homepage__section"]}>
-          <h1 className={classes["homepage__section-title"]}>
-            Titulo de la assad
-          </h1>
+          <h1 className={classes["homepage__section-title"]}>Top Rated</h1>
           <HorizontalList
             url={
               "https://api.themoviedb.org/3/movie/top_rated?api_key=b61e1b3719a9ee56423ad6e473cbf2ab&language=en-US&page=1"
             }
           />
         </div>
-        <div className={`${classes["homepage__section"]}`}>
-          <h1 className={classes["homepage__section-title"]}>
-            Titulo de la sección
-          </h1>
-          <HorizontalList
-            url={
-              "https://api.themoviedb.org/3/movie/top_rated?api_key=b61e1b3719a9ee56423ad6e473cbf2ab&language=en-US&page=2"
-            }
-          />
-        </div>
-        <div className={`${classes["homepage__section"]}`}>
-          <h1 className={classes["homepage__section-title"]}>
-            Titulo de la sección
-          </h1>
-          <HorizontalList
-            url={
-              "https://api.themoviedb.org/3/movie/top_rated?api_key=b61e1b3719a9ee56423ad6e473cbf2ab&language=en-US&page=3"
-            }
-          />
-        </div>
-        <div className={`${classes["homepage__section"]}`}>
-          <h1 className={classes["homepage__section-title"]}>
-            Titulo de la sección
-          </h1>
-          <HorizontalList
-            url={
-              "https://api.themoviedb.org/3/movie/top_rated?api_key=b61e1b3719a9ee56423ad6e473cbf2ab&language=en-US&page=4"
-            }
-          />
-        </div>
 
         <div className={`$classes['homepage__section]`}>
-          <h1 className={classes["homepage__section-title"]}>
-            Titulo de la sección
-          </h1>
+          <h1 className={classes["homepage__section-title"]}>Now Playing</h1>
           <InfiniteList
             url={nowPlayingUrl}
             onScrollLimit={onScrollLimitHandler}
