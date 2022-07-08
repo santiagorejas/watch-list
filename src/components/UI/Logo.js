cb5b6619c3cfbf835bbf4662465112ebe441d048
santiagorejas <santiago.orejasrg@gmail.com>
2022-07-08 11:53:38 -0300

    home search bar UI updated

--- src/components/UI/Logo.js
+++ src/components/UI/Logo.js
@@ -2,15 +2,15 @@ import camera from "../../resources/svg/camera.svg";
 import classes from "./Logo.module.css";
 
 const Logo = (props) => {
-  return (
-    <div
-      className={`${classes.logo} ${props.className} `}
-      onClick={props.onClick}
-    >
-      <img src={camera} />
-      <h1>WatchList</h1>
-    </div>
-  );
+    return (
+        <div
+            className={`${classes.logo} ${props.className} `}
+            onClick={props.onClick}
+        >
+            <img src={camera} alt="watch-list logo" />
+            <h1>WatchList</h1>
+        </div>
+    );
 };
 
 export default Logo;
--- src/components/search/HomeSearch.js
+++ src/components/search/HomeSearch.js
@@ -1,27 +1,27 @@
 import classes from "./HomeSearch.module.css";
 import SearchBar from "./SearchBar";
 import Logo from "../UI/Logo";
-import { useLocation, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const HomeSearch = (props) => {
-  const history = useHistory();
+    const history = useHistory();
 
-  const onSearchHandler = (inputValue) => {
-    history.push({
-      pathname: "search",
-      search: `?name=${inputValue}`,
-    });
-  };
+    const onSearchHandler = (inputValue) => {
+        history.push({
+            pathname: "search",
+            search: `?name=${inputValue}`,
+        });
+    };
 
-  return (
-    <div className={`bor-rad-bg box-shadow-bg ${classes["home-search"]}`}>
-      <Logo className={`${classes["home-search__logo"]}`} />
-      <SearchBar
-        className={`${classes["home-search__search-bar"]}`}
-        onSearch={onSearchHandler}
-      />
-    </div>
-  );
+    return (
+        <div className={`bor-rad-bg box-shadow-bg ${classes["home-search"]}`}>
+            <Logo className={`${classes["home-search__logo"]}`} />
+            <SearchBar
+                className={`${classes["home-search__search-bar"]}`}
+                onSearch={onSearchHandler}
+            />
+        </div>
+    );
 };
 
 export default HomeSearch;
